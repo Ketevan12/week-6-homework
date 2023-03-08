18d99a52f07812a1908c1744d0b7575e4fb077f8
Ketevan12 <kirvk209@newschool.edu>
2023-03-08 17:30:05 -0500

    update

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 console.log('Remove this console log and get started!')
 
-const data = {
-  channels: [
-    // Add your channel data here
-    // {
-    //   title: 'UFOs',
-    //   videoIds: [
-    //     // Add your video IDs here
-    //   ]
-    // }
-  ]
+
+// all available urls
+var Hasan = ["https://www.youtube.com/embed/qeI_wN9KbnE", 	
+            "https://www.youtube.com/embed/_d1aGJZ0yIM",
+						"https://www.youtube.com/embed/9RFWYobuPWQ",
+            "https://www.youtube.com/embed/JPkb3t2AX5I",
+            "https://www.youtube.com/embed/nCwtDtnBPSM"];
+
+
+function loadHasan() {
+  // get iframe element by id
+  var el = document.getElementById('ifr');
+  // get new url
+  var newUrl = Hasan[Math.floor(Math.random()*Hasan.length)];
+  el.src = newUrl;
 }
-<iframe width="560" height="315" src="https://www.youtube.com/embed/l0MqlDbZ_as" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+
+var loadH3 = ["https://youtu.be/embed/UKWp708x-Fk", 	
+									"https://www.youtube.com/embed/T5TLgg4vDUw",
+									"https://www.youtube.com/embed/mllXxyHTzfg"];
+
+                  function loadH3() {
+                    // get iframe element by id
+                    var el = document.getElementById('ifr');
+                    // get new url
+                    var newUrl = H3[Math.floor(Math.random()*H3.length)];
+                    el.src = newUrl;
+                  }
